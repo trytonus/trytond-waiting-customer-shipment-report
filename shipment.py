1a288738cb42e709a32e453e4dda6caa22046fe5
Tarun Bhardwaj <mailme@tarunbhardwaj.com>
2015-11-02 16:23:11 +0530

    Added LICENSE file

--- shipment.py
+++ shipment.py
@@ -2,8 +2,6 @@
 """
     shipment.py
 
-    :copyright: (c) 2015 by Openlabs Technologies & Consulting (P) Limited
-    :license: BSD, see LICENSE for more details.
 """
 from collections import defaultdict
 from openlabs_report_webkit import ReportWebkit
